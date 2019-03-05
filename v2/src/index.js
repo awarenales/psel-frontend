77173e71550b46bc3445329591527f1b180df789
awarenales <awarenales@gmail.com>
2019-03-05 18:33:36 -0300

    666666

--- v2/src/index.js
+++ v2/src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import logo from './logo_branco.png';
 import './index.css';
 
 class Square extends React.Component {
+constructor(props) {
+  super(props);
+  this.state = {
+    value: null,
+  };
+}
+
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button
+      className="square"
+      onClick={() => this.setState({value: 'X'})}
+      >
+        {this.state.value}
       </button>
     );
   }
@@ -14,7 +25,7 @@ class Square extends React.Component {
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i} />;
   }
 
   render() {
@@ -46,6 +57,8 @@ class Board extends React.Component {
 class Game extends React.Component {
   render() {
     return (
+      <div className="header">
+        <Header />
       <div className="game">
         <div className="game-board">
           <Board />
@@ -54,6 +67,17 @@ class Game extends React.Component {
           <div>{/* status */}</div>
           <ol>{/* TODO */}</ol>
         </div>
+      </div>
+      </div>
+    );
+  }
+}
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div className="header">
+        <img src={logo} className="logo" alt="arquivei"/> 
       </div>
     );
   }
